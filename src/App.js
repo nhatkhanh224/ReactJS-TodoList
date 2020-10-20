0c0553ab223e3d0b21ab9233c4c8b60bf66a2e94
nhatkhanh224 <55695197+nhatkhanhl@users.github.com>
2020-10-20 21:30:04 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,258 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import Control from "./components/Control";
+import TaskForm from "./components/TaskForm";
+import _ from 'lodash';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      isDislayForm: false,
+      taskEditing: null,
+      filter: {
+        name: "",
+        status: -1,
+      },
+      keyword: "",
+      sortBy: "name",
+      sortValue: 1,
+    };
+  }
+  componentWillMount() {
+    if (localStorage && localStorage.getItem("tasks")) {
+      var tasks = JSON.parse(localStorage.getItem("tasks"));
+      this.setState({
+        tasks: tasks,
+      });
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  generateId() {
+    return this.s4() + this.s4() + this.s4() + this.s4() + "khanhID";
+  }
+  onToggleForm = () => {
+    if (this.state.isDislayForm && this.state.taskEditing !== null) {
+      this.setState({
+        isDislayForm: true,
+        taskEditing: null,
+      });
+    } else {
+      this.setState({
+        isDislayForm: !this.state.isDislayForm,
+        taskEditing: null,
+      });
+    }
+  };
+  onCloseForm = () => {
+    this.setState({
+      isDislayForm: false,
+    });
+  };
+  onSubmit = (data) => {
+    var { tasks } = this.state;
+
+    //ADD
+    if (data.id === "") {
+      data.id = this.generateId();
+      tasks.push(data);
+    } else {
+      //UPDATE
+      var index = this.findIndex(data.id);
+      tasks[index] = data;
+    }
+
+    this.setState({
+      tasks: tasks,
+      taskEditing: null,
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+  findIndex = (id) => {
+    var { tasks } = this.state;
+    var result = -1;
+    tasks.forEach((task, index) => {
+      if (task.id === id) {
+        result = index;
+      }
+    });
+    return result;
+  };
+  onUpdateStatus = (id) => {
+    var { tasks } = this.state;
+    // var index = this.findIndex(id);
+    var index=_.findIndex(tasks,(task)=>{
+      return task.id===id;
+    })
+    if (index !== -1) {
+      tasks[index].status = !tasks[index].status;
+      this.setState({
+        tasks: tasks,
+      });
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  };
+  onDelete = (id) => {
+    var { tasks } = this.state;
+    var index = this.findIndex(id);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      this.setState({
+        tasks: tasks,
+      });
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+    this.onCloseForm();
+  };
+  onShowForm = () => {
+    this.setState({
+      isDislayForm: true,
+    });
+  };
+  onUpdate = (id) => {
+    var { tasks } = this.state;
+    var index = this.findIndex(id);
+    var taskEditing = tasks[index];
+    this.setState({
+      taskEditing: taskEditing,
+    });
+    this.onShowForm();
+  };
+  onFilter = (filterName, filterStatus) => {
+    filterStatus = parseInt(filterStatus, 10);
+    this.setState({
+      filter: {
+        name: filterName.toLowerCase(),
+        status: filterStatus,
+      },
+    });
+  };
+  onSearch = (keyword) => {
+    this.setState({
+      keyword: keyword,
+    });
+  };
+  onSort = (sortBy, sortValue) => {
+    this.setState({
+      sortBy: sortBy,
+      sortValue: sortValue,
+    });
+  };
+  render() {
+    var {
+      tasks,
+      isDislayForm,
+      taskEditing,
+      filter,
+      keyword,
+      sortBy,
+      sortValue,
+    } = this.state;
+    if (filter) {
+      if (filter.name) {
+        tasks = tasks.filter((task) => {
+          return task.name.toLowerCase().indexOf(filter.name) !== -1;
+        });
+      }
+      tasks = tasks.filter((task) => {
+        if (filter.status === -1) {
+          return task;
+        } else {
+          return task.status === (filter.status === 1 ? true : false);
+        }
+      });
+    }
+
+    if (keyword) {
+      tasks = tasks.filter((task) => {
+        return task.name.toLowerCase().indexOf(keyword) !== -1;
+      });
+    }
+    if (sortBy === "name") {
+      tasks.sort((a, b) => {
+        if (a.name > b.name) return sortValue;
+        else if (a.name < b.name) return -sortValue;
+        else return 0;
+      });
+    } else {
+      tasks.sort((a, b) => {
+        if (a.status > b.status) return -sortValue;
+        else if (a.status < b.status) return sortValue;
+        else return 0;
+      });
+    }
+    var elmTaskForm = isDislayForm ? (
+      <TaskForm
+        onSubmit={this.onSubmit}
+        onCloseForm={this.onCloseForm}
+        task={taskEditing}
+      />
+    ) : (
+      ""
+    );
+    return (
+      <div>
+        <div className="container">
+          <div className="text-center">
+            <h1>Quản Lý Công Việc</h1>
+            <hr />
+          </div>
+          <div className="row">
+            <div
+              className={
+                isDislayForm ? "col-xs-4 col-sm-4 col-md-4 col-lg-4" : ""
+              }
+            >
+              {/* TaskForm */}
+              {elmTaskForm}
+            </div>
+            <div
+              // Form input
+              className={
+                isDislayForm
+                  ? "col-xs-8 col-sm-8 col-md-8 col-lg-8"
+                  : "col-xs-12 col-sm-12 col-md-12 col-lg-12"
+              }
+            >
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this.onToggleForm}
+              >
+                <span className="fa fa-plus mr-5"></span>Thêm Công Việc
+              </button>
+
+              {/* Control */}
+              <Control
+                onSearch={this.onSearch}
+                onSort={this.onSort}
+                sortBy={sortBy}
+                sortValue={sortValue}
+              />
+              <div className="row mt-15">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  {/* Tasklist */}
+                  <TaskList
+                    tasks={tasks}
+                    onUpdateStatus={this.onUpdateStatus}
+                    onDelete={this.onDelete}
+                    onUpdate={this.onUpdate}
+                    onFilter={this.onFilter}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
